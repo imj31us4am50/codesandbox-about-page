16fe47a60f990a1a0716be85610b7cf73cb8ccc8
Volosenco Andreea-Laura <91089388+imj31us4am50@users.noreply.github.com>
2022-04-16 14:02:37 +0300

    upd

--- mainaboutpage/upperContent.js
+++ mainaboutpage/upperContent.js
@@ -3,8 +3,11 @@ import upperContentCSS from "/mainaboutpage/upperContent.css";
 export default function upperContent() {
   return (
     <div className="container-fluid">
-      <div className="text-white description-of-about p-2 m-0">
-        <h1>Despre noi</h1>
+      <div className="d-flex text-white for-about p-2 m-0">
+        <div className="container-of-desc">
+          <h1>Despre noi</h1>
+        </div>
+        <div className="container-of-pic"></div>
       </div>
     </div>
   );
