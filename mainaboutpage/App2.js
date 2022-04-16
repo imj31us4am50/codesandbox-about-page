50c58e001096dcd852516e88a40fac800da5374f
Volosenco Andreea-Laura <91089388+imj31us4am50@users.noreply.github.com>
2022-04-16 14:22:11 +0300

    upd

--- mainaboutpage/App2.js
+++ mainaboutpage/App2.js
@@ -1,13 +1,11 @@
 import "./styles2.css";
 import bootstrap from "bootstrap"; // eslint-disable-line no-unused-vars
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import Header from "/navbar/App1.js";
 import UpperContent from "/mainaboutpage/upperContent.js";
 
 export default function App1() {
   return (
     <div className="container-fluid pope">
-      <Header />
       <UpperContent />
     </div>
   );
