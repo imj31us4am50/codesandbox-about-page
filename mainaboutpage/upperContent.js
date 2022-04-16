f1607001a8f2d55415b87e1430697fd4be2d0a08
Volosenco Andreea-Laura <91089388+imj31us4am50@users.noreply.github.com>
2022-04-16 14:01:07 +0300

    upd

--- mainaboutpage/upperContent.js
+++ mainaboutpage/upperContent.js
@@ -1,5 +1,11 @@
 import upperContentCSS from "/mainaboutpage/upperContent.css";
 
 export default function upperContent() {
-  return <div className="container-fluid"></div>;
+  return (
+    <div className="container-fluid">
+      <div className="text-white description-of-about p-2 m-0">
+        <h1>Despre noi</h1>
+      </div>
+    </div>
+  );
 }
