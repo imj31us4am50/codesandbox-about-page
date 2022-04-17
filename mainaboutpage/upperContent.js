46dfe7b69f7cfe9a0f9721e000170d90ce00c78f
Volosenco Andreea-Laura <91089388+imj31us4am50@users.noreply.github.com>
2022-04-17 16:49:40 +0300

    upd

--- mainaboutpage/upperContent.js
+++ mainaboutpage/upperContent.js
@@ -2,20 +2,21 @@ import upperContentCSS from "/mainaboutpage/upperContent.css";
 
 export default function upperContent() {
   return (
-    <div className="container-fluid m-0 p-0" style={upperContentCSS}>
-      <div className="d-flex for-about p-2 m-0">
-        <div className="container-of-desc w-100">
-          <h1 className="text-center mx-auto">
-            Despre <span className="green">Nature Smarter</span>
-          </h1>
-        </div>
-        <div className="other-container w-100">
-          <div className="collage">
-            <img
-              src="https://imj31us4am50.github.io/imgs/animal-collage.png"
-              className="the-image"
-            />
-          </div>
+    <div
+      className="container-fluid m-0 p-0 d-flex pt-5 for-about"
+      style={upperContentCSS}
+    >
+      <div className="container-of-desc w-100">
+        <h1 className="text-center mx-auto mt-3">
+          Despre <span className="green">Nature Smarter</span>
+        </h1>
+      </div>
+      <div className="other-container w-100">
+        <div className="collage">
+          <img
+            src="https://imj31us4am50.github.io/imgs/animal-collage.png"
+            className="the-image"
+          />
         </div>
       </div>
     </div>
