46672a3eccc471d1b717d7b7714ca1b05ea98990
Volosenco Andreea-Laura <91089388+imj31us4am50@users.noreply.github.com>
2022-04-18 10:02:19 +0300

    upd

--- mainaboutpage/App2.js
+++ mainaboutpage/App2.js
@@ -2,11 +2,13 @@ import "./styles2.css";
 import bootstrap from "bootstrap"; // eslint-disable-line no-unused-vars
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import UpperContent from "/mainaboutpage/upperContent.js";
+import LowerContent from "/mainaboutpage/lowerContent.js";
 
 export default function App1() {
   return (
     <div className="container-fluid pope m-0 p-0">
       <UpperContent />
+      <LowerContent />
     </div>
   );
 }
--- mainaboutpage/lowerContent.js
+++ mainaboutpage/lowerContent.js
@@ -2,6 +2,18 @@ import lowerContentCSS from "/mainaboutpage/lowerContent.css";
 
 export default function lowerContent() {
   return (
-    <div className="container-fluid m-0 p-0" style={lowerContentCSS}></div>
+    <div
+      className="container-fluid m-0 p-0 lower-content"
+      style={lowerContentCSS}
+    >
+      <div className="container-of-video m-0 p-0">
+        <video
+          src="https://imj31us4am50.github.io/NSEDITEDVIDS/animal-video-for-home-page.mp4"
+          className="home-main-video"
+          muted
+          autoPlay
+        />
+      </div>
+    </div>
   );
 }
