70d17bde16e7cb808db6bcd726c5135061da7702
Volosenco Andreea-Laura <91089388+imj31us4am50@users.noreply.github.com>
2022-04-18 07:31:50 +0300

    upd

---
+++ mainaboutpage/lowerContent.js
@@ -0,0 +1,7 @@
+import lowerContentCSS from "/mainaboutpage/lowerContent.css";
+
+export default function lowerContent() {
+  return (
+    <div className="container-fluid m-0 p-0" style={lowerContentCSS}></div>
+  );
+}
