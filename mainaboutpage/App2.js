062fb21a443029d1ec413b79699da265826c437b
Volosenco Andreea-Laura <91089388+imj31us4am50@users.noreply.github.com>
2022-04-21 10:14:07 +0300

    upd

--- mainaboutpage/App2.js
+++ mainaboutpage/App2.js
@@ -3,12 +3,16 @@ import bootstrap from "bootstrap"; // eslint-disable-line no-unused-vars
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import UpperContent from "/mainaboutpage/upperContent.js";
 import LowerContent from "/mainaboutpage/lowerContent.js";
+import AskAQuestion from "/mainaboutpage/AskAQuestion.js";
+import QuestionForm from "/mainaboutpage/questionForm.js";
 
 export default function App1() {
   return (
     <div className="container-fluid pope m-0 p-0">
       <UpperContent />
       <LowerContent />
+      <AskAQuestion />
+      <QuestionForm />
     </div>
   );
 }
---
+++ mainaboutpage/questionForm.js
@@ -0,0 +1,36 @@
+import questionFormCSS from "/mainaboutpage/questionForm.css";
+
+export default function questionForm() {
+  return (
+    <div className="questionform m-5 p-2 mx-auto" style={questionFormCSS}>
+      <div className="inputspart d-flex align-items-start">
+        <div className="information-part p-4 w-100">
+          <input
+            type="text"
+            placeholder="Nume"
+            required
+            className="nameinput d-block p-2 m-2 mb-3"
+          ></input>
+          <input
+            type="email"
+            placeholder="Email"
+            required
+            className="emailinput d-block p-2 m-2 mb-3"
+          ></input>
+        </div>
+        <div className="message-part p-4 w-100">
+          <textarea
+            placeholder="Mesajul tău"
+            required
+            className="messageinput d-block p-2 m-2"
+          ></textarea>
+        </div>
+      </div>
+      <div className="submitbutton text-center">
+        <button className="btn opziqe text-white m-3" type="submit">
+          Trimite mesaj
+        </button>
+      </div>
+    </div>
+  );
+}
