e235d8e6c9b09bfa6c6f31559b67fd16b53e2538
Volosenco Andreea-Laura <91089388+imj31us4am50@users.noreply.github.com>
2022-04-21 10:22:11 +0300

    upd

--- mainaboutpage/questionForm.js
+++ mainaboutpage/questionForm.js
@@ -2,35 +2,37 @@ import questionFormCSS from "/mainaboutpage/questionForm.css";
 
 export default function questionForm() {
   return (
-    <div className="questionform m-5 p-2 mx-auto" style={questionFormCSS}>
-      <div className="inputspart d-flex align-items-start">
-        <div className="information-part p-4 w-100">
-          <input
-            type="text"
-            placeholder="Nume"
-            required
-            className="nameinput d-block p-2 m-2 mb-3"
-          ></input>
-          <input
-            type="email"
-            placeholder="Email"
-            required
-            className="emailinput d-block p-2 m-2 mb-3"
-          ></input>
+    <div className="questionform mt-5 p-1 mx-auto" style={questionFormCSS}>
+      <form>
+        <div className="inputspart d-flex align-items-start">
+          <div className="information-part p-4 w-100">
+            <input
+              type="text"
+              placeholder="Nume"
+              required
+              className="nameinput d-block p-2 m-2 mb-3"
+            ></input>
+            <input
+              type="email"
+              placeholder="Email"
+              required
+              className="emailinput d-block p-2 m-2 mb-3"
+            ></input>
+          </div>
+          <div className="message-part p-4 w-100">
+            <textarea
+              placeholder="Mesajul tău"
+              required
+              className="messageinput d-block p-2 m-2"
+            ></textarea>
+          </div>
         </div>
-        <div className="message-part p-4 w-100">
-          <textarea
-            placeholder="Mesajul tău"
-            required
-            className="messageinput d-block p-2 m-2"
-          ></textarea>
+        <div className="submitbutton text-center">
+          <button className="btn opziqe text-white m-3" type="submit">
+            Trimite mesaj
+          </button>
         </div>
-      </div>
-      <div className="submitbutton text-center">
-        <button className="btn opziqe text-white m-3" type="submit">
-          Trimite mesaj
-        </button>
-      </div>
+      </form>
     </div>
   );
 }
